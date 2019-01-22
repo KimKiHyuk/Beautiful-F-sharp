36cd5c6f2dfd106b8b10d13bf2334460b303dfca
key kim <keyhyuk.kim@gmail.com>
2019-01-22 13:28:12 +0000

    Add example of curry

--- FSNetCore/src/App/Program.fs
+++ FSNetCore/src/App/Program.fs
@@ -7,6 +7,27 @@
 open System
 
 [<EntryPoint>]
-let main argv = 
-        printfn "Hello World from F#!"
-        0 // return 0
+let main argv =
+        let add arg_x arg_y arg_z =
+                arg_x + arg_y + arg_z
+
+        let result = add 1 2 3
+
+        let temp_1 = add 1
+
+        printfn "temp_1 (add 1) : %A" temp_1
+
+        let temp_2 = temp_1 2
+
+        printfn "temp_2 (add 1 2) : %A" temp_2
+
+        let resultByCurry = temp_2 3
+
+        printfn "resultByCurry (add 1 2 3) : %A" resultByCurry
+
+        if result = resultByCurry then
+            printfn "Same result"
+        else
+            printfn "different result"
+
+        0 // return
